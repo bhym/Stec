88729c3cde5a5c668decd7978f6f78a2b48045f9
arfalas <bruno.haymele@szn.it>
2020-09-22 13:45:36 +0200

    added script to convert from laTeX to docx

---
+++ report/to_docx.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cp $1 input.tex
+
+echo "Converting pdf images in pngs"
+mogrify -format png imgs/*.pdf
+echo "Adjusting the tex so that it points to png rather than pdfs"
+sed -i  's/\.pdf/.png/g' input.tex
+
+echo "Converting the tex document to a standalone output.docx"
+pandoc input.tex -o output.docx
+
+rm -f input.tex imgs/*.png
