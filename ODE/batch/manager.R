18babca62934452c77ada9e74d38fd5788d18f0a
arfalas <bruno.haymele@szn.it>
2020-10-01 23:10:54 +0200

    updated manager for new sweep

--- ODE/batch/manager.R
+++ ODE/batch/manager.R
@@ -8,19 +8,21 @@ library("deSolve")
 source("model.R")
 source("parms.R")
 
- range_dfac <- seq(0.1, 0.9, 0.05)
- range_f1is <- seq(1, 9, 1)
- range_r0p  <- seq(0.1, 5, 0.05)
- range_tga  <- seq(1, 9, 1)
+ range_dfac <- seq(0.1, 0.9, 0.1)
+ range_r0p  <- seq(0.1, 3, 0.1)
+ range_tga  <- seq(1, 6, 1)
+ range_f1is <- seq(1, 6, 1)
+ range_alp  <- seq(0.01, 0.2, 0.01)
 
  subspace <- crossing(randfac = range_dfac,
+                      ranr0p  = range_r0p,
+                      rantga  = range_tga,
                       ranf1is = range_f1is,
-                      rantga = range_tga,
-                      ranr0p  = range_r0p)
+                      ranalp  = range_alp)
 
 sweep_ <- subspace %>%
-  mutate(outs = future_pmap(list(randfac, ranf1is, ranr0p, 1, rantga),
-                            function(x, y, z, j, u) mollo(x, y, z, j, u)
+  mutate(outs = future_pmap(list(randfac, ranr0p, 1, rantga, ranf1is, ranalp),
+                            function(x, y, z, j, u, b) mollo(x, y, z, j, u,b )
                             )
         )
 
