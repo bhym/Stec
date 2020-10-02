bc651247176b58384fec17a0dbdfdc42d0c726d9
arfalas <bruno.haymele@szn.it>
2020-10-02 17:01:45 +0200

    changed GA start parameter to 1

--- ODE/batch/parms.R
+++ ODE/batch/parms.R
@@ -4,7 +4,7 @@ input <- list(
            alpha_                 = 0.012,
            dfac                   = 0.8,
            end_time               = 10,
-           growth_block_start     = 2,
+           growth_block_start     = 1,
            growth_block_length    = 3,
            f1_injection_step      = 3,
            growth_rate_p          = 1,
