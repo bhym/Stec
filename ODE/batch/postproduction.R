2b0871ea142a0b0f068770278f59b48e989acb85
arfalas <bruno.haymele@szn.it>
2020-10-02 19:23:34 +0200

    removed extra surfaces, subsetted tables

--- ODE/batch/postproduction.R
+++ ODE/batch/postproduction.R
@@ -16,12 +16,13 @@ tab_parsub <- function(dat, x_, y_) {
     select(!!x, !!y, rantga, logo) %>%
     group_by(rantga) %>%
     nest %>%
-    mutate(matrici = map(data, ~pivot_wider(.x, names_from = !!y,
+    mutate(matrici = map(data, ~pivot_wider(.x, names_from = !!x,
                                             values_from = logo) %>%
-    column_to_rownames(x_))) %>%
+    arrange(desc(!!y)) %>%
+    column_to_rownames(y_))) %>%
     ungroup() %>%
     select(-data) %>%
-    pwalk(function(rantga, matrici, a = y_, b = x_)
+    pwalk(function(rantga, matrici, a = x_, b = y_)
           kable(matrici, "latex", booktabs = T, digits = 2) %>%
           save_kable(paste("../../report/tbls/",
                            a, "-", b, "_",
@@ -60,30 +61,13 @@ plb <- alp_vs_r0p %>%
          ylab(expression(r[p] : r[f1])) +
          coord_cartesian(ylim = c(1, 3))
 
-plc <- f1i_vs_r0p %>%
-         pl_parsub(ranf1is, ranr0p) +
-         facet_wrap(~rantga) +
-         xlab("Day of F1 appearence") +
-         ylab(expression(r[p] : r[f1])) +
-         coord_cartesian(ylim = c(1, 3))
 
-pld <- dfa_vs_f1i %>%
-         pl_parsub(randfac, ranf1is) +
-         facet_wrap(~rantga) +
-         xlab("P gametogenesis-induced death") +
-         ylab("Day of F1 appearence")
-
-
-tab_parsub(dfa_vs_r0p, "randfac", "ranr0p")
-tab_parsub(dfa_vs_f1i, "randfac", "ranf1is")
-tab_parsub(f1i_vs_r0p, "ranf1is", "ranr0p")
-
-fint <- unique(lsw_a$ranr0p)[c(10, 15, 20, 25, 30)]
-sint <- unique(lsw_a$ranalp)[c(1, 5, 10, 15, 20)]
+fint <- unique(lsw_a$ranr0p) [seq(1, 21, len = 9)]
+sint <- unique(lsw_a$ranalp) [seq(1, 20, len = 9)]
 co_alp_vs_r0p <- alp_vs_r0p %>% filter(ranalp %in% sint, ranr0p %in% fint)
+co_dfa_vs_r0p <- dfa_vs_r0p %>% filter(ranr0p %in% fint)
 tab_parsub(co_alp_vs_r0p, "ranalp",  "ranr0p")
+tab_parsub(co_dfa_vs_r0p, "randfac", "ranr0p")
 
 ggsave("../../report/imgs/a.pdf", pla, device = cairo_pdf)
 ggsave("../../report/imgs/b.pdf", plb, device = cairo_pdf)
-ggsave("../../report/imgs/c.pdf", plc, device = cairo_pdf)
-ggsave("../../report/imgs/d.pdf", pld, device = cairo_pdf)
