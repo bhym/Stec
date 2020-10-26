bb593a02c2cd422ba8be3f978cfa93a62dd436b3
arfalas <bruno.haymele@szn.it>
2020-10-26 20:10:06 +0100

    changed little things to make the conversion better

--- report/to_docx.sh
+++ report/to_docx.sh
@@ -7,6 +7,8 @@ echo "Adjusting the tex so that it points to png rather than pdfs"
 sed -i  's/\.pdf/.png/g' input.tex
 
 echo "Converting the tex document to a standalone output.docx"
-pandoc input.tex -o output.docx
+pandoc input.tex --mathjax \
+                 --bibliography=paper_integration.bib \
+                 -o output.docx 
 
 rm -f input.tex imgs/*.png
